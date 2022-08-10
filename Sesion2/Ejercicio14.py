396d10e2e147d3cd0fe22708e50fc16199fec096
Replit user <>
2022-08-10 03:07:37 +0000

    segundo commit de sesion2

---
+++ Sesion2/Ejercicio14.py
@@ -0,0 +1,12 @@
+'''
+*********** Curso de programación acelerada en Python ************
+Date 04-08-2022
+File: sesion2/ejercicio13.py
+Autor: ..............
+Action: suma de 10 primeros numeros
+'''
+palabra = input("Escriba una palabra :")
+n = 1
+while n <= 10:
+ print(palabra)
+ n += 1
---
+++ Sesion2/Ejercicio17.py
@@ -0,0 +1,12 @@
+'''
+*********** Curso de programación acelerada en Python ************
+Date 09-08-2022
+File: sesion2/ejercicio13.py
+Autor: yo
+Action: Tablas
+'''
+i=1
+n = int(input("Introduce un numero (entero positivo): "))
+for i in range(10):
+  print(n,"*",i,"=",n*i)
+ 
